9c7edf940e68b1fd1f4d5913a11184bbb542e4a9
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-09 02:51:21 +0100

    add admin field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_08_132540) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_09_015001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "username", null: false
+    t.integer "attack", default: 100
+    t.integer "defense", default: 100
+    t.integer "steps", default: 0
+    t.integer "wood", default: 500
+    t.integer "stone", default: 500
+    t.integer "metal", default: 500
+    t.integer "level", default: 1
+    t.integer "experience", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +38,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_08_132540) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
