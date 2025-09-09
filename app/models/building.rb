4ed4fa320b7929dc4b7304ec64023388b3e5120c
yan buxes <yanbuxes@gmail.com>
2025-09-09 14:49:14 -0300

    building index new create

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,4 +3,9 @@ class Building < ApplicationRecord
   has_many :troops, dependent: :destroy
 
   BUILDING_TYPES = %w[castle barracks quarry sawmill mine]
+
+  validates :building_type, presence: true, inclusion: { in: BUILDING_TYPES }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_nil: true
+
+  validates :building_type, uniqueness: { scope: :profile_id, message: "already exists" }
 end
