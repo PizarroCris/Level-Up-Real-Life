b4c5cd56f22bb51fcf36cf48b60f3f876b9789ce
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-09 04:21:59 +0100

    Create all Profile_Policy

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -1,14 +1,41 @@
 class ProfilePolicy < ApplicationPolicy
-  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
-  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
-  # In most cases the behavior will be identical, but if updating existing
-  # code, beware of possible changes to the ancestors:
-  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+  class Scope < Scope
+    def resolve
+      if user.admin
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
 
-  class Scope < ApplicationPolicy::Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  def show?
+    true
+  end
+
+  def create?
+    user.profile.nil?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    owner?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    record.user == user || user.admin
   end
 end
