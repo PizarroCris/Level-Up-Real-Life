9138867570c426df45bfbdce031bba48980e7a46
Caio Figueiredo <47697760+CAiAuM@users.noreply.github.com>
2025-09-11 17:03:16 -0300

    exibir tipos de tropas, seus poderes, e adicionar botao de criar

--- app/models/troop.rb
+++ app/models/troop.rb
@@ -2,17 +2,24 @@ class Troop < ApplicationRecord
   belongs_to :building
   has_one :profile, through: :building
 
+  before_validation :set_stats
+
+  validates :troop_type, presence: true
+
   TROOP_STATS = {
     archer: { attack: 15, defense: 5 },
     cavalry: { attack: 20, defense: 10 },
     swordsman: { attack: 10, defense: 15 }
   }
 
-  def attack_value
-    TROOP_STATS[troop_type.to_sym][:attack] * (1 + level * 0.01)
-  end
+  private
 
-  def defense_value
-    TROOP_STATS[troop_type.to_sym][:defense] * (1 + level * 0.01)
+  def set_stats
+    return unless troop_type.present?
+    skills = TROOP_STATS[troop_type.to_sym]
+    if skills
+      self.attack = skills[:attack] * (1 + level * 0.01)
+      self.defense = skills[:defense] * (1 + level * 0.01)
+    end
   end
 end
