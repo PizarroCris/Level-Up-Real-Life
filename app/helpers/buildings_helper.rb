5c049af19172a2b5bb615a39ea653c164310e097
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-12 14:52:48 +0100

    resolve conflict

--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -3,6 +3,17 @@ module BuildingsHelper
     case building.building_type.downcase
     when "barrack"
       building_troops_path(building)
+
+    when "mine", "sawmill", "quarry"
+      kind = building.building_type.downcase
+      resource = building.resources.find_by(kind: kind)
+
+      if resource
+        building_resource_path(building, resource)
+      else
+        new_building_resource_path(building, resource: { kind: kind })
+      end
+
     else
       building_path(building)
     end
