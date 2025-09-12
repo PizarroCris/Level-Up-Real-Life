5232717cec38e9374859d2436cb10324bdd26d3b
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-12 02:26:07 +0100

    add upgrade btn and some js

---
+++ app/javascript/controllers/building_menu_controller.js
@@ -0,0 +1,51 @@
+// app/javascript/controllers/building_menu_controller.js
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu", "detailsLink", "upgradeFormContainer"]
+
+  connect() {
+    this.boundHideOnClick = this.hideOnClick.bind(this)
+    document.addEventListener("click", this.boundHideOnClick)
+    window.addEventListener("resize", this.boundHideOnClick) 
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.boundHideOnClick)
+    window.removeEventListener("resize", this.boundHideOnClick)
+  }
+  show(event) {
+    event.stopPropagation()
+
+    const buildingWrapper = event.currentTarget
+    const showUrl = buildingWrapper.dataset.buildingShowUrl
+    const upgradeUrl = buildingWrapper.dataset.buildingUpgradeUrl
+    const canUpgrade = buildingWrapper.dataset.buildingUpgradeCost === "true"
+
+    this.detailsLinkTarget.href = showUrl
+
+    const upgradeForm = this.upgradeFormContainerTarget.querySelector("form")
+    if (upgradeForm) {
+      upgradeForm.action = upgradeUrl
+    }
+    
+    this.upgradeFormContainerTarget.style.display = canUpgrade ? 'block' : 'none'
+
+    const containerRect = this.element.getBoundingClientRect();
+    const buildingRect = buildingWrapper.getBoundingClientRect();
+    const relativeTop = buildingRect.bottom - containerRect.top -30;
+    const relativeLeft = buildingRect.left - containerRect.left;
+
+    this.menuTarget.style.top = `${relativeTop}px`;
+    this.menuTarget.style.left = `${relativeLeft}px`;
+    this.menuTarget.classList.add("is-visible")
+  }
+
+  hideOnClick(event) {
+    if (event.type === 'click' && this.element.contains(event.target)) {
+      return
+    }
+    this.menuTarget.classList.remove("is-visible")
+  }
+}
