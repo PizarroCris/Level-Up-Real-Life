1ba14e36be813a3dc493cc403d5520ebb21800ee
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-13 22:43:32 +0100

    feat: create guild model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_12_145539) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_13_213558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_12_145539) do
     t.integer "price_in_steps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "guilds", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.bigint "leader_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["leader_id"], name: "index_guilds_on_leader_id"
   end
 
   create_table "plots", force: :cascade do |t|
@@ -108,6 +117,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_12_145539) do
   add_foreign_key "buildings", "profiles"
   add_foreign_key "equipment", "equipment_items"
   add_foreign_key "equipment", "profiles"
+  add_foreign_key "guilds", "profiles", column: "leader_id"
   add_foreign_key "profiles", "users"
   add_foreign_key "resources", "buildings"
   add_foreign_key "troops", "buildings"
