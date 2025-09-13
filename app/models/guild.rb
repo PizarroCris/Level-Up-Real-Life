3da1a0f4cbb4894581957f6cfb6839cb9b14e947
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-14 00:06:14 +0100

    feat: add validations and associations to guild model

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -1,3 +1,15 @@
 class Guild < ApplicationRecord
-  belongs_to :leader
+  belongs_to :leader, class_name: 'Profile'
+  has_many :guild_memberships
+  has_many :profiles, through: :guild_memberships
+
+  validates :name,
+          presence: true,
+          uniqueness: { case_sensitive: false },
+          length: { in: 4..12 }
+  validates :tag, 
+          presence: true, 
+          uniqueness: { case_sensitive: false },
+          length: { in: 3..5 },
+          format: { with: /\A[A-Z0-9]+\z/, message: "can only contain uppercase letters and numbers" }
 end
