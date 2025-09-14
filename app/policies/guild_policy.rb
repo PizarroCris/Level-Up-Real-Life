aa5544731cf9627234783e239721268d3ca4658a
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-14 01:03:14 +0100

    feat: add guild policies

---
+++ app/policies/guild_policy.rb
@@ -0,0 +1,41 @@
+class GuildPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.profile.guild.nil?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    owner?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    record.leader.user == user || user.admin?
+  end
+end
