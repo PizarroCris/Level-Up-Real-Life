de6a954ea22eaa4510bd970ef44d72d60aa61c43
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-16 00:29:20 +0100

    feat: change battle to accept attacker choose troops amount per attack

--- app/services/battle_simulator_service.rb
+++ app/services/battle_simulator_service.rb
@@ -1,54 +1,64 @@
 class BattleSimulator
-  def initialize(attacker_profile, defender_profile)
-    @attacker = attacker_profile
+  def initialize(attacking_army, defender_profile)
+    @attacking_army = attacking_army
+    @defender_army = defender_profile.troops
+
+    @attacker = attacking_army.first.profile
     @defender = defender_profile
+    
     @battle_log = []
   end
 
   def call
-    while @attacker.can_fight? && @defender.can_fight?
-      perform_attack_round(@attacker, @defender)
-      break unless @defender.can_fight?
-      perform_attack_round(@defender, @attacker)
+    @battle_log << "A battle begins between #{@attacker.username} and #{@defender.username}!"
+
+    while @attacking_army.reload.any? && @defender_army.reload.any?
+      
+      perform_attack_round(@attacking_army, @defender_army, @attacker, @defender)
+      
+      break if @defender_army.reload.empty?
+
+      perform_attack_round(@defender_army, @attacking_army, @defender, @attacker)
     end
 
-    if @attacker.can_fight?
-      winner = @attacker
-      @battle_log << "#{@attacker.username} is victorious!"
-    else
-      winner = @defender
-      @battle_log << "#{@defender.username} is victorious!"
-    end
+    determine_final_winner
 
-    return { winner: winner, log: @battle_log.join("\n") }
+    return { winner: @winner, log: @battle_log.join("\n") }
   end
 
   private
 
-  def perform_attack_round(current_attacker, current_defender)
-    attack_power = current_attacker.total_attack
-    defense_power = current_defender.total_defense
-
+  def perform_attack_round(current_attacker_army, current_defender_army, attacker_profile, defender_profile)
+    attack_power = current_attacker_army.sum(&:attack)
+    defense_power = current_defender_army.sum(&:defense)
     damage = (attack_power - defense_power).max(0)
+    
+    losses = calculate_losses(current_defender_army, damage)
 
-    losses = calculate_losses(current_defender.troops, damage)
+    @battle_log << "#{attacker_profile.username}'s army attacks with #{attack_power} power, dealing #{damage} damage!"
+    @battle_log << "#{defender_profile.username}'s army suffers #{losses} casualties."
 
-    @battle_log << "#{current_attacker.username} attacks with #{attack_power} power against #{current_defender.username}'s #{defense_power} defense, dealing #{damage} damage!"
-    @battle_log << "This results in #{losses} losses for #{current_defender.username}!"
-
-    current_defender.troops.order(:level).limit(losses).destroy_all
+    current_defender_army.order(:level).limit(losses).destroy_all
   end
 
-  def calculate_losses(army_troops, total_damage)
-    return 0 if army_troops.empty?
-
-    total_defense_points_of_army = army_troops.map(&:defense).sum
-    average_defense_per_troop = total_defense_points_of_army.to_f / army_troops.size
-
-    return army_troops.size if average_defense_per_troop.zero?
+  def calculate_losses(army, total_damage)
+    return 0 if army.empty?
     
-    number_of_losses = (total_damage / average_defense_per_troop).floor
+    total_hp_of_army = army.sum(&:defense)
+    average_hp_per_troop = total_hp_of_army.to_f / army.size
+    return army.size if average_hp_per_troop.zero?
+    
+    number_of_losses = (total_damage / average_hp_per_troop).floor
+    return number_of_losses.clamp(0, army.size)
+  end
 
-    return number_of_losses.clamp(0, army_troops.size)
+  def determine_final_winner
+    if @attacking_army.reload.any?
+      @winner = @attacker
+      @battle_log << "#{@attacker.username} is victorious!"
+    else
+      @winner = @defender
+      @battle_log << "#{@defender.username} is victorious!"
+    end
   end
 end
