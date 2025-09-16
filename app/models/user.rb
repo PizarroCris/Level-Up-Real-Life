ff81c153e70674c30e00e7851d6b46ba65202b30
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-16 19:36:28 +0100

    feat: add location btn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one :profile, dependent: :destroy
+  after_create :create_user_profile
+
+  private
+
+  def create_user_profile
+    empty_spot = MapPlot.where.missing(:profile).first
+    self.create_profile!(
+      username: self.email.split('@').first,
+      map_plot: empty_spot
+    )
+  end
 end
