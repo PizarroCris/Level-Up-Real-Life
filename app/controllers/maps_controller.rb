1a60e455fda160e8182df2ef81d8e9d95b0be07b
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-16 18:59:30 +0100

    make dragged map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,9 @@
 class MapsController < ApplicationController
-  before_action :authenticate_user!
-
   def index
-    @profiles_with_castles = policy_scope(Profile).includes(:map_plot, :buildings)
+    @castles = policy_scope(Building).where(building_type: 'castle').includes(profile: :map_plot)
     @monsters = policy_scope(WorldMonster)
-    @available_plots = policy_scope(MapPlot).left_outer_joins(:profile).where(profiles: { id: nil })
+    @available_plots = policy_scope(MapPlot).where.missing(:profile)
   end
 end
+
+
