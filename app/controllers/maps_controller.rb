bca160ad4b88335ebcfdbf574112d19b73743289
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-16 16:08:19 +0100

    add world map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,8 +2,8 @@ class MapsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @profiles_with_castles = Profile.includes(:map_plot, :buildings).where.not(map_plot: nil)
-    @monsters = WorldMonster.all
-    @available_plots = MapPlot.left_outer_joins(:profile).where(profiles: { id: nil })
+    @profiles_with_castles = policy_scope(Profile).includes(:map_plot, :buildings)
+    @monsters = policy_scope(WorldMonster)
+    @available_plots = policy_scope(MapPlot).left_outer_joins(:profile).where(profiles: { id: nil })
   end
 end
