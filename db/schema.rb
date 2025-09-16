77369f245f0f0c3e1d265d1ccacabf993e2b57c3
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-16 10:11:00 +0100

    feat: add WorldMonster model and mibgration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_15_171117) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_16_090038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -154,6 +154,25 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_15_171117) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "world_monsters", force: :cascade do |t|
+    t.string "name"
+    t.integer "level"
+    t.integer "pos_x"
+    t.integer "pos_y"
+    t.integer "hp", default: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "world_resources", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "pos_x"
+    t.integer "pos_y"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "battles", "profiles", column: "attacker_id"
