6d0d8e4b764d6dc3f72b10de7d23d7e21c3b62a3
yan buxes <yanbuxes@gmail.com>
2025-09-17 16:04:06 -0300

    info popup cost

---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  toggle(event) {
+    event.stopPropagation()
+    const open = this.menuTarget.classList.toggle("is-visible")
+
+    // Update aria-expanded on wrapper
+    const wrapper = this.element.querySelector(".building-wrapper")
+    if (wrapper) wrapper.setAttribute("aria-expanded", open ? "true" : "false")
+  }
+
+  hideOnDocumentClick = (e) => {
+    if (!this.element.contains(e.target)) {
+      this.menuTarget.classList.remove("is-visible")
+      const wrapper = this.element.querySelector(".building-wrapper")
+      if (wrapper) wrapper.setAttribute("aria-expanded", "false")
+    }
+  }
+
+  connect() {
+    document.addEventListener("click", this.hideOnDocumentClick)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.hideOnDocumentClick)
+  }
+}
