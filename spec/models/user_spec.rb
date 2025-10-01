96025d33e8def4c310fe4fbaa8d979e4f9cafa09
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-10-01 11:26:32 +0100

    feat: add User model creation callback tests and fix RSpec config

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,35 @@ RSpec.describe User, type: :model do
       expect(User.reflect_on_association(:profile).macro).to eq(:has_one)
     end
   end
+
+  describe '#create_user_profile' do
+    let!(:empty_map_plot) { create(:map_plot) }
+    let!(:central_plot) { create(:plot, name: 'Plot 1') }
+
+    context 'when a new user is created' do
+      it 'creates a profile for the user' do
+        user = create(:user)
+        expect(user.profile).to be_present
+        expect(user.profile.username).to eq(user.email.split('@').first)
+      end
+
+      it 'assigns an empty MapPlot to the new profile' do
+        user = create(:user)
+        expect(user.profile.map_plot).to eq(empty_map_plot)
+        expect(empty_map_plot.reload.profile).to eq(user.profile)
+      end
+
+      it 'creates a level 1 castle for the new profile on Plot 1' do
+        user = create(:user)
+        expect(user.profile.buildings.count).to eq(1)
+
+        castle = user.profile.buildings.first
+
+        expect(castle.building_type).to eq('castle')
+        expect(castle.level).to eq(1)
+
+        expect(castle.plot).to eq(central_plot)
+      end
+    end
+  end
 end
